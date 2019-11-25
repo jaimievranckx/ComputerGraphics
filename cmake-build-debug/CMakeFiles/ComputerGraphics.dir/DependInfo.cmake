eafc8bd0dcef8cc347d49dd1d5ccc6e1a1e60bc4
Jaimie Vranckx <jaimie.vranckx@student.uantwerpen.be>
2019-11-25 01:09:02 +0100

    square added

---
+++ cmake-build-debug/CMakeFiles/ComputerGraphics.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/jaimi/Documents/ComputerGraphics/camera.cpp" "C:/Users/jaimi/Documents/ComputerGraphics/cmake-build-debug/CMakeFiles/ComputerGraphics.dir/camera.cpp.obj"
+  "C:/Users/jaimi/Documents/ComputerGraphics/lightSource.cpp" "C:/Users/jaimi/Documents/ComputerGraphics/cmake-build-debug/CMakeFiles/ComputerGraphics.dir/lightSource.cpp.obj"
+  "C:/Users/jaimi/Documents/ComputerGraphics/main.cpp" "C:/Users/jaimi/Documents/ComputerGraphics/cmake-build-debug/CMakeFiles/ComputerGraphics.dir/main.cpp.obj"
+  "C:/Users/jaimi/Documents/ComputerGraphics/object.cpp" "C:/Users/jaimi/Documents/ComputerGraphics/cmake-build-debug/CMakeFiles/ComputerGraphics.dir/object.cpp.obj"
+  "C:/Users/jaimi/Documents/ComputerGraphics/ray.cpp" "C:/Users/jaimi/Documents/ComputerGraphics/cmake-build-debug/CMakeFiles/ComputerGraphics.dir/ray.cpp.obj"
+  "C:/Users/jaimi/Documents/ComputerGraphics/scene.cpp" "C:/Users/jaimi/Documents/ComputerGraphics/cmake-build-debug/CMakeFiles/ComputerGraphics.dir/scene.cpp.obj"
+  "C:/Users/jaimi/Documents/ComputerGraphics/sphere.cpp" "C:/Users/jaimi/Documents/ComputerGraphics/cmake-build-debug/CMakeFiles/ComputerGraphics.dir/sphere.cpp.obj"
+  "C:/Users/jaimi/Documents/ComputerGraphics/square.cpp" "C:/Users/jaimi/Documents/ComputerGraphics/cmake-build-debug/CMakeFiles/ComputerGraphics.dir/square.cpp.obj"
+  "C:/Users/jaimi/Documents/ComputerGraphics/vector3.cpp" "C:/Users/jaimi/Documents/ComputerGraphics/cmake-build-debug/CMakeFiles/ComputerGraphics.dir/vector3.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ComputerGraphics.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ComputerGraphics.dir/camera.cpp.obj"
+  "CMakeFiles/ComputerGraphics.dir/lightSource.cpp.obj"
+  "CMakeFiles/ComputerGraphics.dir/main.cpp.obj"
+  "CMakeFiles/ComputerGraphics.dir/object.cpp.obj"
+  "CMakeFiles/ComputerGraphics.dir/ray.cpp.obj"
+  "CMakeFiles/ComputerGraphics.dir/scene.cpp.obj"
+  "CMakeFiles/ComputerGraphics.dir/sphere.cpp.obj"
+  "CMakeFiles/ComputerGraphics.dir/square.cpp.obj"
+  "CMakeFiles/ComputerGraphics.dir/vector3.cpp.obj"
+  "ComputerGraphics.exe"
+  "ComputerGraphics.exe.manifest"
+  "ComputerGraphics.pdb"
+  "libComputerGraphics.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ComputerGraphics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
